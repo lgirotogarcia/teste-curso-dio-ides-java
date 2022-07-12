e5fa0d1a7b995562d2b9519684d63f5385ded221
Leonardo Garcia <leonardogirotogarcia@gmail.com>
2022-07-12 18:17:40 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		/*int a = 2;
+		int b = 3;
+		
+		System.out.println("Hello world " + (a+b));*/
+	
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+	}
+
+}
